942f3e4c9a777869b99ec1602719ee03e5be6e87
JulianBarth <julian@bartholo.me>
2024-11-22 22:19:14 +0100

    Adding my project to git repo

---
+++ secrets_example.py
@@ -0,0 +1,4 @@
+# secrets.py
+
+TIBBER_API_KEY =  "_xyz"
+WALLBOX_API_KEY = "https://data.wattpilot.io/api/v1/direct_json?e=WxIcvzAIxLQx7gQUQY6YYTKmLJz-5_PoFrWPXFF4UtJciYZuQGBH4Vwvblof6nJrl-ZnGJR29aB7ACg"
